991f9c962f435b9840d45e886fe89435af15143d
deolv12 <deanandameuthia@gmail.com>
2025-02-20 12:13:45 +0700

    praktikum 1

--- README.md
+++
@@ -1,7 +0,0 @@
-# Pemrograman Web 2 
-## Repositori Praktikum Pemrograman Web 2
-
-- Nama : Deananda Meuthia
-- NIM : 0110124186
-- Rombel : SI11
-
---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variabel
+
+$nama = 'Deananda Meuthia';
+$umur = 20;
+$berat = 53;
+
+echo 'Nama saya ' . $nama;
+echo ' <br/>umur :' . $umur . 'Tahun';
+echo ' <br/> berat :' . $berat . ' kg.';
+?>
